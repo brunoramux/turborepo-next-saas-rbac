94fe5ef86b3484893d16b80f340384e7ce429291
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-01-01 20:04:09 -0300

    feat: create user endpoint

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
+import { hashPassword } from '../../../lib/encrypt'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -10,17 +12,61 @@ export async function createAccount(app: FastifyInstance) {
         body: z.object({
           name: z.string(),
           email: z.email(),
-          password: z.string().min(6),
+          password: z.string(),
         }),
         response: {
-          201: z.string(),
+          201: z.object({
+            newUser: z.object({
+              id: z.string(),
+              name: z.string().nullable(),
+              email: z.string(),
+            }),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
         },
       },
     },
-    (request, response) => {
-      const body = request.body
-      console.log('Creating user:', body)
-      return response.status(201).send(JSON.stringify(body))
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: 'Email already in use. Please use a different email.',
+        })
+      }
+
+      try {
+        const hashedPassword = await hashPassword(password)
+        const newUser = await prisma.user.create({
+          data: {
+            name,
+            email,
+            passwordHash: hashedPassword,
+          },
+        })
+
+        if (newUser) {
+          reply.status(201).send({
+            newUser: {
+              id: newUser.id,
+              name: newUser.name,
+              email: newUser.email,
+            },
+          })
+        }
+      } catch (e) {
+        return reply.status(400).send({
+          message: 'Error creating user. Please try again.',
+        })
+      }
     }
   )
 }
