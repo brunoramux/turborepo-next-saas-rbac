c078db16e686eb3f286af8dc91f1a386058715ab
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-01-01 22:46:59 -0300

    Feat: add api docs using Fastify Swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,11 +1,14 @@
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -13,6 +16,22 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.JS SaaS with RBAC',
+      description: 'Full Stack SaaS App',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(createAccount)
 
